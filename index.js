6aade0abea7d5c503c5b61b1970b1a4334a679fe
Anthony Smith <tony.smith@calrec.com>
2023-02-28 20:18:56 +0000

    refactor: make async and poll data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import noble from "@abandonware/noble";
 
 const BT_DEVICE_NAME = "BT-TH-F258CF8C";
+const INTERVAL = 1000;
 
 noble.on("stateChange", (state) => {
   if (state === "poweredOn") {
@@ -13,7 +14,7 @@ noble.on("stateChange", (state) => {
 });
 
 const initiateScan = () => {
-  noble.startScanning([], true); // any service UUID, allow duplicates
+  noble.startScanning([], true);
 
   noble.on("scanStart", () => {
     console.log("Scanning for BLE peripherals...");
@@ -26,132 +27,57 @@ const initiateScan = () => {
   noble.on("warning", (message) => {
     console.log("Warning:", message);
   });
+
+  noble.on("discover", async (peripheral) => {
+    if (peripheral.advertisement.localName == BT_DEVICE_NAME) {
+      // connect to the peripheral
+      peripheral.once("connect", () => {
+        console.log(
+          "Connected to peripheral:",
+          peripheral.advertisement.localName
+        );
+      });
+      // disconnect from the peripheral
+      peripheral.once("disconnect", () => {
+        console.log(
+          "Disconnected from peripheral:",
+          peripheral.advertisement.localName
+        );
+      });
+      // stop scanning
+      await noble.stopScanningAsync();
+      // connect to the peripheral
+      await peripheral.connectAsync();
+      // discover the services and characteristics of the peripheral
+      const { characteristics } =
+        await peripheral.discoverAllServicesAndCharacteristicsAsync([
+          "fff0",
+          "ffd0",
+        ]);
+      // find the characteristics we need
+      const rx = characteristics.find((c) => c.uuid === "fff1");
+      const tx = characteristics.find((c) => c.uuid === "ffd1");
+      // subscribe to the rx characteristic
+      rx.on("data", (data) => {
+        let payload = Buffer.from(data);
+        let numCells = payload.readInt16LE(4);
+        let volts = [];
+        for (let x = 1; x <= numCells; x++) {
+          let volt = payload.readInt16LE(x * 2 + 4) / 10;
+          volts.push(volt);
+          console.log(volts.join(", "));
+        }
+        console.log("Received data");
+      });
+      // enable notifications
+      await rx.notifyAsync(true);
+      // write to the tx characteristic
+      setInterval(() => {
+        tx.writeAsync(
+          Buffer.from([0x30, 0x03, 0x13, 0x88, 0x00, 0x11, 0x05, 0x49]),
+          true
+        );
+      }, [INTERVAL]);
+    }
+  });
 };
-
-noble.on("discover", (peripheral) => {
-  peripheral.once("connect", () => {
-    console.log("Connected to peripheral:", peripheral.advertisement.localName);
-  });
-
-  peripheral.once("disconnect", () => {
-    console.log(
-      "Disconnected from peripheral:",
-      peripheral.advertisement.localName
-    );
-  });
-
-  if (peripheral.advertisement.localName == BT_DEVICE_NAME) {
-    console.log("Found peripheral:", peripheral.advertisement.localName);
-    peripheral.connect((error) => {
-      if (error) {
-        console.log("Error connecting to peripheral:", error);
-      } else {
-        peripheral.discoverServices([], (error, services) => {
-          if (error) {
-            console.log("Error discovering services:", error);
-          } else {
-            services.forEach((service) => {
-              if (service.uuid == "fff0") {
-                console.log("TX");
-                console.log("Found service:", service.uuid);
-                service.discoverCharacteristics(
-                  [],
-                  (error, characteristics) => {
-                    if (error) {
-                      console.log("Error discovering characteristics:", error);
-                    } else {
-                      characteristics.forEach((characteristic) => {
-                        console.log(
-                          "Found characteristic:",
-                          characteristic.uuid
-                        );
-                        if (characteristic.uuid == "fff1") {
-                          characteristic.on("data", (data, isNotification) => {
-                            let payload = Buffer.from(data);
-                            let numCells = payload.readInt16LE(4);
-                            let volts = [];
-                            for (let x = 1; x <= numCells; x++) {
-                              let volt = payload.readInt16LE(x * 2 + 4) / 10;
-                              volts.push(volt);
-                              console.log(volts.join(", "));
-                            }
-
-                            console.log("Received data:", data.toString("hex"));
-                          });
-                          characteristic.notify(true, (error) => {
-                            if (error) {
-                              console.log(
-                                "Error enabling notifications:",
-                                error
-                              );
-                            } else {
-                              console.log("Enabled notifications");
-                            }
-                          });
-                        }
-                      });
-                    }
-                  }
-                );
-              } else if (service.uuid == "ffd0") {
-                console.log("RX");
-                console.log("Found service:", service.uuid);
-                service.discoverCharacteristics(
-                  [],
-                  (error, characteristics) => {
-                    if (error) {
-                      console.log("Error discovering characteristics:", error);
-                    } else {
-                      characteristics.forEach((characteristic) => {
-                        console.log(
-                          "Found characteristic:",
-                          characteristic.uuid
-                        );
-                        if (characteristic.uuid == "ffd1") {
-                          //   characteristic.write(
-                          //     Buffer.from([
-                          //       0x30, 0x03, 0x13, 0xb2, 0x00, 0x06, 0x65, 0x4a,
-                          //     ]),
-                          //     true,
-                          //     (error) => {
-                          //       if (error) {
-                          //         console.log(
-                          //           "Error writing to characteristic:",
-                          //           error
-                          //         );
-                          //       } else {
-                          //         console.log("Wrote to characteristic");
-                          //       }
-                          //     }
-                          //   );
-
-                          characteristic.write(
-                            Buffer.from([
-                              0x30, 0x03, 0x13, 0x88, 0x00, 0x11, 0x05, 0x49,
-                            ]),
-                            true,
-                            (error) => {
-                              if (error) {
-                                console.log(
-                                  "Error writing to characteristic:",
-                                  error
-                                );
-                              } else {
-                                console.log("Wrote to characteristic");
-                              }
-                            }
-                          );
-                        }
-                      });
-                    }
-                  }
-                );
-              }
-            });
-          }
-        });
-      }
-    });
-    noble.stopScanning();
-  }
-});
